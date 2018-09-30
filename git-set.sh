4655cee4fe4d6d997e6e95ccb1202b9e3f7519d6
Margin <hujun1@genomics.cn>
2018-09-30 11:27:02 +0800

    packet

---
+++ git-set.sh
@@ -0,0 +1,9 @@
+git config --global user.name "Margin"
+git config --global user.email hujun1@genomics.cn
+
+git config --global push.default simple
+
+# git clone ssh://git@gitlab.genomics.cn:2200/blc/osp-cloud-ops.git
+# git clone ssh://git@gitlab.genomics.cn:2200/blc/osp-undercloud.git
+# git clone ssh://git@gitlab.genomics.cn:2200/blc/osp-overcloud.git
+# git clone ssh://git@gitlab.genomics.cn:2200/blc/osp-kolla.git
